f2ce44fd61733beb7c6d3ca52ebd68a953f81c97
skmajumder <skmajumder.cse@gmail.com>
2023-06-10 23:48:19 +0600

    Users POST

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wnk3oaq.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -28,10 +27,29 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    
+    const database = client.db("sclsDB");
+    const usersCollection = database.collection("users");
+
+    /**
+     * User Router
+     */
+
+    // * Insert User info into database
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser && existingUser.email) {
+        return res.send({ message: "User Exists" });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+    /**
+     * User Router End
+     */
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -39,7 +57,6 @@ async function run() {
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-    // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
